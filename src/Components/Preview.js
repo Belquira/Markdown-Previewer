fd0aa5f3760935ecd9c82771579f3be5f750d01c
Anabel <anabelpazb@gmail.com>
2019-04-05 20:14:25 -0300

    implementada la funcionalidad para Markdown, componente convertido a Clase para metodos lifecycle

--- src/Components/Preview.js
+++ src/Components/Preview.js
@@ -1,19 +1,42 @@
-import React from "react";
+import React, { Component } from "react";
 import { connect } from "react-redux";
+import marked from "marked";
 
 const mapStateToProps = (state) => {
     return {
         textPreview: state.text
     }
 }
-const Preview = ({ textPreview }) => {
-    return (
-        <div>
-            Preview
-                <p>{ textPreview }</p>
-        </div>
-    );
-}
+
+
+class Preview extends Component {
+
+    markedPreview = () => {
+        let markedOptions = {
+            gfm: true
+          }
+        let content = this.props.textPreview;
+        let markedText = marked(content, markedOptions);
+        document.getElementById("preview").innerHTML = markedText;
+    }
+    
+    componentDidMount() {
+        this.markedPreview();
+    }
+
+    componentDidUpdate() {
+        this.markedPreview();
+    }
+
+    render() {
+    
+        return (
+            <div id="preview"></div>
+        );
+    }
+
+} 
+
 
 const ConnectedComponent = connect(mapStateToProps, null)(Preview);
 
