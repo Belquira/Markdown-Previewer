0a4bbae5676cfc41bd2e4a8278a776c2de1616da
Anabel <anabelpazb@gmail.com>
2019-04-13 06:06:11 -0400

    Updates

---
+++ precache-manifest.0bc78740c5a67dd2405d94f09888d285.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d5b1733e4e2bf3b997b9",
+    "url": "/Markdown-Previewer/static/js/runtime~main.01fd9653.js"
+  },
+  {
+    "revision": "b5ff310c875a64450152",
+    "url": "/Markdown-Previewer/static/js/main.b750b84e.chunk.js"
+  },
+  {
+    "revision": "b9edf2c464b50fc4bcf4",
+    "url": "/Markdown-Previewer/static/js/2.230bf70f.chunk.js"
+  },
+  {
+    "revision": "b5ff310c875a64450152",
+    "url": "/Markdown-Previewer/static/css/main.e45678cf.chunk.css"
+  },
+  {
+    "revision": "76e76926117a169225a9c9c5bdcb7a78",
+    "url": "/Markdown-Previewer/index.html"
+  }
+];
